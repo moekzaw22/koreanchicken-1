da02030288473139a40d5eb729e929c76fbcc093
moekzaw22 <50523951+moekzaw22@users.noreply.github.com>
2023-01-24 22:31:08 +0630

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,7 +1,4 @@
 <?php 
-	$connection = new mysqli("localhost","root","","KoreanChicken");
-
-	if (!$connection) {
-    die("Unable to Connect database: " . mysqli_connect_error());
-}
+	$connection=new mysqli("localhost","root","","KoreanChicken");
+	if(!$connection) {die("Unable to Connect database: " . mysqli_connect_error());}
  ?>
